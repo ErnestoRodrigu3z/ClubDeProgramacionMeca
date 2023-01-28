8dd4c688974a0493796779549643ec0419ff755d
Ernesto Corona <ernesto_rodriguezc2002@outlook.com>
2023-01-27 18:54:41 -0800

    Primera semana terminada

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+base = 0
+altura = 0
+area = 0
+print ("Ingresa la altura del triangulo: ", end="")
+altura = float (input())
+print ("Ingresa la base del triangulo: ", end = "")
+base = float (input())
+area = (base * altura) / 2
+print (str (area) + " cm2")
+
+
